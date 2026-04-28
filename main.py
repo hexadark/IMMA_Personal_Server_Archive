1131ad2667b547aeeed134c494fa0dcc65ab3104
Tae Eun Kwon <rnjsxodms12@gmail.com>
2026-04-28 20:35:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,17 @@ from sqlalchemy.orm import sessionmaker
 
 app = FastAPI()
 
+# ---------------------------
+# CORS 설정
+# ---------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-        "http://localhost:8080",
-        "http://127.0.0.1:8080",
-    ],
+    allow_origins=["*"],  # <--- 이 부분을 ["*"] 로 변경하세요!
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
